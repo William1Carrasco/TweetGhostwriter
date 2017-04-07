9a55121d3c8f141997d475f4cff3445227d08668
William1Carrasco <wcarrasco@mail.csuchico.edu>
2017-04-06 22:24:14 -0700

    Add files via upload

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,7 +1,24 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["Always keep", "The sun shines on", "You must not forget", "Lending a hand to"];
+  var body = ["your heart on a sleve", "your true love", "those who give so freely";
+  "those close to you", "those you love"];
+  var end = ["at the end of the day", "when the sunsets"];
+  randIndex = randomUpTo((intro.length-1));
+  sentence = intro[randIndex];
+  randIndex = randomUpTo((body.length-1));
+  sentence = sentence+" "+body[randIndex];
+  randIndex = randomUpTo((end.length-1));
+  sentence = sentence+" "+end[randIndex];
+  document.getElementById("tweet").innerHTML=sentence;
 }
+
+
+
+
+
 
 //Generates a random whole number between 0 and max (inclusive)
 function randomUpTo(max)
